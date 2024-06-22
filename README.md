1f7ee9aa75e3537d5f15325ff8b43317df2165cc
onath <hatice1759@gmial.com>
2024-06-22 23:58:11 +0200

    plotting women and men participation and success in games

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Programming_Project_Olympic_Data
+# OLYMPIC DATA ANALYSIS
+
+**COLUMNS**
+1. **ID:** identifier for each athlete.
+2. **NAME:**  The full name of the athlete.
+3. **SEX:** The gender of the athlete, represented as 'M' for male and 'F' for female.
+4. **AGE:** The age of the athlete at the time of the Olympics.
+5. **HEIGHT:** The height of the athlete in centimeters.
+6. **WEIGHT:** The weight of the athlete in kilograms.
+7. **TEAM:** The country the athlete represents.
+8. **NOC:** The National Olympic Committee (NOC) code for the country the athlete represents.
+9. **GAMES:** The edition of the Olympics the athlete participated in, including the year and the season (Summer or Winter).
+10. **YEAR:** The year of the Olympics.
+11. **SEASON:** The season of the Olympics, either Summer or Winter.
+12. **CITY:** The host city of the Olympics.
+13. **SPORT:** The sport the athlete competed in.
+14. **EVENT:** The specific event within the sport that the athlete competed in.
+15. **MEDAL:** The type of medal won by the athlete, if any (Gold, Silver, Bronze, or NaN if no medal was won).
--- functions.py
+++ functions.py
@@ -1,6 +1,40 @@
-#################### PRINT UNIQUE VALUES #################### 
+from sklearn.ensemble import RandomForestRegressor
+
+#################### PRINT UNIQUE VALUES #####################
 def print_unique_values(df):
     for column in df.columns:
         unique_values = df[column].unique()
         print(f"Unique values in column '{column}': {unique_values}")
-############################################################
+##############################################################
+
+
+########## IMPUTE MISSING VALUES WITH RANDOM FOREST ########## 
+def impute_missing_values_with_random_forest(df, column_name):
+    # Select only numerical columns (excluding the target column)
+    numerical_columns = df.select_dtypes(include=['int64', 'float64']).columns.tolist()
+    if column_name in numerical_columns:
+        numerical_columns.remove(column_name)
+
+    # Exclude columns with NaN values
+    numerical_columns = [col for col in numerical_columns if not df[col].isna().any()]
+
+    # Splitting the data into two parts: one where target column is missing and one where it's not
+    df_with_target = df[df[column_name].notna()] #the target column is not NaN: These rows provide 
+                        # valuable information because they contain actual data that the model can learn from
+    df_without_target = df[df[column_name].isna()] #rows where the target column is NaN and needs to be imputed 
+                                # (using the information from the rows where the target column is not empty.)
+
+    # Prepare the features (X) and target (y) using only numerical columns
+    x = df_with_target[numerical_columns]
+    y = df_with_target[column_name]
+
+    # Create and train the model
+    model = RandomForestRegressor(random_state=42)
+    model.fit(x, y)
+
+    # Predicting the missing values
+    predicted_values = model.predict(df_without_target[numerical_columns])
+
+    # Fill in the missing values in the original DataFrame
+    df.loc[df[column_name].isna(), column_name] = predicted_values
+    return df
